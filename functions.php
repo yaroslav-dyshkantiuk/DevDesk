db244be07cc654337f50c2e67c61e9e77cb99ba8
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2023-09-14 02:20:33 +0300

    Connection of necessary plugins to the topic

--- functions.php
+++ functions.php
@@ -9,6 +9,55 @@
 
 require get_template_directory() . '/inc/widget-about.php';
 require get_template_directory() . '/inc/metaboxes.php';
+require_once get_template_directory() . '/inc/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'devdesk_register_required_plugins' );
+
+
+function devdesk_register_required_plugins() {
+
+	$plugins = array(
+
+		// This is an example of how to include a plugin bundled with a theme.
+		array(
+			'name'               => 'Devdesk Core', // The plugin name.
+			'slug'               => 'devdesk-core', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/plugins/devdesk-core.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '1.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
+		array(
+			'name'      => 'Advanced Custom Fields (ACF)',
+			'slug'      => 'advanced-custom-fields',
+			'required'  => true,
+		),
+
+		array(
+			'name'      => 'Redux Framework',
+			'slug'      => 'redux-framework',
+			'required'  => true,
+		),
+
+	);
+
+	$config = array(
+		'id'           => 'devdesk',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
 
 
 function devdesk_paginate($query){
